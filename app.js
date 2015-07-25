083ae8aea0d10db7b0de7daa6532a30d83c93e80
Kevin <kevinbaran4@gmail.com>
2015-07-24 23:09:19 -0400

    Bug Fix Update
    • Fixed 'undefind' usernames
    • Fixed 'multi-clienting' (IP Check, so people can't spam usernames -
    for now)
    • Other small changes

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var express = require('express'),
 	port = Number(process.env.PORT || 3000),
 	colors = require('colors'),
 	server = '[Server]'.white.bold,
-	users = [];
+	users = [],
+	ips = [];
 
 app.use('/images', express.static('images'));
 app.use('/css', express.static('css'));
@@ -17,25 +18,32 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
 	socket.on('new user', function (data, callback) {
 		var user = /^[\w]{1,15}$/; // Username can only contain letters, numbers and can be 1-15 characters long
+		var userip = getIPAddresses().toString().trim();
 		if (users.indexOf(data) != -1) { // Username is already taken
 			callback(false);
-		} else if (data == null) { // No username was entered
+		} else if (ips.indexOf(userip) != -1) { // IP is already in use
 			callback(false);
-		} else if (!user.test(data)){ // Username has invalid characters
+		} else if (!user.test(data)) { // Username has invalid characters
+			callback(false);
+		} else if (data == null) { // No username was entered
 			callback(false);
 		} else {
 			callback(true);
 			socket.users = data;
+			socket.ips = userip;
 			users.push(socket.users);
+			ips.push(socket.ips)
 			updateNicknames();
-			console.log(server + ' New User: ' + data);
+			console.log(server + ' New User: ' + data + ' - IP: ' + userip);
 			io.emit('chat message', '<font color="#5E97FF"><b>[Server]</b> ' + data + ' has joined</font><br/>');
 		}
 	});
 
 	socket.on('disconnect', function () {
 		if (!socket.users) return;
+		if (!socket.ips) return;
 		users.splice(users.indexOf(socket.users), 1); // Remove user from list
+		ips.splice(ips.indexOf(ips.users), 1); // Remove ip from list
 		updateNicknames();
 		console.log(server + ' User Left: ' + socket.users);
 		io.emit('chat message', '<font color="#4E7ACC"><b>[Server]</b> ' + socket.users + ' has left</font><br/>');
@@ -47,29 +55,49 @@ io.on('connection', function (socket) {
 
 	socket.on('chat message', function (msg) {
 		if (!msg == "") {
-			if (msg.indexOf("<")) {
-				io.emit('chat message', '<b>' + socket.users + '</b>: ' + msg + '<br/>');
-				console.log(('[User] ').gray.bold + socket.users + ': ' + msg);
-			} else {
-				var newmsg = msg.replace(/</g, '&lt;');
-				io.emit('chat message', '<b>' + socket.users + '</b>: ' + newmsg + '<br/>');
-				console.log(('[User] ').gray.bold + socket.users + ': ' + msg);
+			if (!socket.users == "") {
+				if (msg.indexOf("<")) {
+					io.emit('chat message', '<b>' + socket.users + '</b>: ' + msg + '<br/>');
+					console.log(('[User] ').gray.bold + socket.users + ': ' + msg);
+				} else {
+					var newmsg = msg.replace(/</g, '&lt;');
+					io.emit('chat message', '<b>' + socket.users + '</b>: ' + newmsg + '<br/>');
+					console.log(('[User] ').gray.bold + socket.users + ': ' + msg);
+				}
 			}
 		}
 	});
+
+	function getIPAddresses() { // When using a no-ip service, this gets the wrong IP, but can still be useful for detecting the same ip
+
+		var ipAddresses = [];
+
+		var interfaces = require('os').networkInterfaces();
+		for (var devName in interfaces) {
+			var iface = interfaces[devName];
+			for (var i = 0; i < iface.length; i++) {
+				var alias = iface[i];
+				if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+					ipAddresses.push(alias.address);
+				}
+			}
+		}
+
+		return ipAddresses;
+	}
 });
 
 var stdin = process.stdin, stdout = process.stdout;
 
 stdin.resume();
 stdin.on('data', function (data) {
-	var trim = data.toString().trim();
-	if (trim == 'shutdown') {
+	var input = data.toString().trim();
+	if (input == 'shutdown') {
 		io.emit('shutdown', '<font color="#910000"><b>[Server]</b> The server has shut down</font><br/>');
 		console.log(server + ' Shutting down...');
 		process.exit();
 	} else {
-		io.emit('chat message', '<font color="#5E97FF"><b>[Server]</b> ' + trim + '</font><br/>');
+		io.emit('chat message', '<font color="#5E97FF"><b>[Server]</b> ' + input + '</font><br/>');
 	}
 });
 
