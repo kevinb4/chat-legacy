a0ceafd6397d8bbae309a69da85f7fd28e2076fc
Kevin <kevinbaran4@gmail.com>
2015-07-25 22:11:58 -0400

    IP Check Update
    • Added IP Check (to fix username spamming)
    • Fixed formatting issues
    • Changed startup method
    • Added more line comments

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express'),
 	app = express(),
-	http = require('http').Server(app),
-	io = require('socket.io')(http),
 	port = Number(process.env.PORT || 3000),
+	io = require('socket.io').listen(app.listen(port)),
 	colors = require('colors'),
 	server = '[Server]'.white.bold,
-	users = [];
+	users = [],
+	ips = [];
+
+console.log(server + ' listening @ localhost:' + port);
 
 app.use('/images', express.static('images'));
 app.use('/css', express.static('css'));
@@ -17,7 +19,10 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
 	socket.on('new user', function (data, callback) {
 		var user = /^[\w]{1,15}$/; // Username can only contain letters, numbers and can be 1-15 characters long
+		var userip = socket.request.connection.remoteAddress; // Get IP - Unshortend
 		if (users.indexOf(data) != -1) { // Username is already taken
+			callback(false);
+		} else if (ips.indexOf(userip) != -1) { // IP is already in use
 			callback(false);
 		} else if (!user.test(data)) { // Username has invalid characters
 			callback(false);
@@ -26,33 +31,37 @@ io.on('connection', function (socket) {
 		} else {
 			callback(true);
 			socket.users = data;
+			socket.ips = userip;
 			users.push(socket.users);
+			ips.push(socket.ips)
 			updateNicknames();
-			console.log(server + ' New User: ' + data);
+			console.log(server + ' New User: ' + data + ' IP: ' + userip);
 			io.emit('chat message', '<font color="#5E97FF"><b>[Server]</b> ' + data + ' has joined</font><br/>');
 		}
 	});
 
 	socket.on('disconnect', function () {
 		if (!socket.users) return;
+		if (!socket.ips) return;
 		users.splice(users.indexOf(socket.users), 1); // Remove user from list
+		ips.splice(ips.indexOf(ips.users), 1); // Remove ip from list
 		updateNicknames();
 		console.log(server + ' User Left: ' + socket.users);
 		io.emit('chat message', '<font color="#4E7ACC"><b>[Server]</b> ' + socket.users + ' has left</font><br/>');
 	});
 
 	function updateNicknames() {
-		io.sockets.emit('usernames', users);
+		io.sockets.emit('usernames', users); // Updates the userlist
 	}
 
 	socket.on('chat message', function (msg) {
-		if (!msg == "") {
-			if (!socket.users == "") {
-				if (msg.indexOf("<")) {
+		if (!msg == "") { // Check to make sure a message was entered
+			if (!socket.users == "") { // Check to make sure the client has a username
+				if (msg.indexOf("<")) { // Check if the user is trying to use html
 					io.emit('chat message', '<b>' + socket.users + '</b>: ' + msg + '<br/>');
 					console.log(('[User] ').gray.bold + socket.users + ': ' + msg);
 				} else {
-					var newmsg = msg.replace(/</g, '&lt;');
+					var newmsg = msg.replace(/</g, '&lt;'); // Changes the character to show as a <, but not work with HTML
 					io.emit('chat message', '<b>' + socket.users + '</b>: ' + newmsg + '<br/>');
 					console.log(('[User] ').gray.bold + socket.users + ': ' + msg);
 				}
@@ -61,20 +70,20 @@ io.on('connection', function (socket) {
 	});
 });
 
+//--------------
+// CMD Commands
+//--------------
+
 var stdin = process.stdin, stdout = process.stdout;
 
 stdin.resume();
 stdin.on('data', function (data) {
-	var input = data.toString().trim();
-	if (input == 'shutdown') {
+	var input = data.toString().trim(); // Take out any unecessary spaces
+	if (input == 'shutdown') { // Shutdown command
 		io.emit('shutdown', '<font color="#910000"><b>[Server]</b> The server has shut down</font><br/>');
 		console.log(server + ' Shutting down...');
 		process.exit();
-	} else {
+	} else { // Anything else that's entered is sent as a server message
 		io.emit('chat message', '<font color="#5E97FF"><b>[Server]</b> ' + input + '</font><br/>');
 	}
-});
-
-http.listen(port, function () {
-	console.log(server + ' listening @ localhost:' + port);
 });
--- js/chat.js
+++ js/chat.js
@@ -14,45 +14,45 @@ $(document).ready(function () {
 });
 
 function login() {
-    socket.emit('new user', username.val(), function (data) {
-        if (data) { // Have the server check if the username is valid
-            loginform.fadeOut("slow", function () {
-                chat.fadeIn("slow", function () { }); // Fade into the chatbox
-            });
-        } else {
-            alert('There is something wrong with your username. Please check the following:\r\n-You entered a username\r\n-Your username can only contain letters and numbers (1-15 characters long)\r\n-Someone else already has that username');
-        }
-    });
+	socket.emit('new user', username.val(), function (data) {
+		if (data) { // Have the server check if the username is valid
+			loginform.fadeOut("slow", function () {
+				chat.fadeIn("slow", function () { }); // Fade into the chatbox
+			});
+		} else {
+			alert('There is something wrong with your username. Please check the following:\r\n-You entered a username\r\n-Your username can only contain letters and numbers (1-15 characters long)\r\n-Someone else already has that username\r\n-You are already logged in (on the same IP address)');
+		}
+	});
 }
 
 set.click(function () { // Clicking the send button
-    login();
+	login();
 });
 
 username.keypress(function (e) {
-    if (e.which == 13) { // Pressing Enter
-        login();
-    }
+	if (e.which == 13) { // Pressing Enter
+		login();
+	}
 });
 
 function sendMessage() {
-    socket.emit('chat message', reply.val()); // Emit the message
-    reply.val(""); // Clear the reply box
+	socket.emit('chat message', reply.val()); // Emit the message
+	reply.val(""); // Clear the reply box
 }
 
 send.click(function () { // Clicking the send button
-    sendMessage();
+	sendMessage();
 });
 
 reply.keypress(function (e) { // Checks for keys being pressed
-    if (e.which == 13) { // Pressing Enter
-        sendMessage();
-    }
+	if (e.which == 13) { // Pressing Enter
+		sendMessage();
+	}
 });
 
 socket.on('chat message', function (msg) { // When a message is emitted...
 	textarea.append(msg); // ...the message is shown in the chatbox
-    chatbox.scrollTop($(chatbox).get(0).scrollHeight); // Scroll down
+	chatbox.scrollTop($(chatbox).get(0).scrollHeight); // Scroll down
 });
 
 socket.on('usernames', function (data) {
